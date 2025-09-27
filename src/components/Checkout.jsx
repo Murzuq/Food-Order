9a3a8b7620a1ec467fdf6b5a1c184e8afd2796fe
Murzuq Alabi <murzuqalabi@gmail.com>
2025-09-27 14:20:14 +0200

    Added Checkout submit Functionality

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -20,16 +20,36 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); //{email: test@example.com}
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
-        <p>Total Amoun: {currencyFormatter.format(cartTotal)}</p>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" id="full-name" />
+        <Input label="Full Name" id="name" />
         <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" id="street" />
-        <div className="control-rol">
+        <div className="control-row">
           <Input label="Postal Code" id="postal-code" />
           <Input label="City" id="city" />
         </div>
